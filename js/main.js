f40c86b8fa6567dfcd3a2384b1683ac9153f2e72
DESKTOP-7OQUT6O\leoso <leo.solo@mail.ru>
2018-01-17 19:09:35 +0700

    +правка текста, отправки, бесконечные цвета, добавление блоков с любой стороны, но 2 бага

--- js/main.js
+++ js/main.js
@@ -2,62 +2,107 @@
 
 document.addEventListener("DOMContentLoaded", function(){       //Страница загрузилась
     $('.mainBlock').on('click', function(e){
-        var top = e.pageY;
-        main.getHeight($(this), top);
+        var y = e.pageY,
+            x = e.pageX;
+        main.getHeight($(this), y, x);
     });
 });
 
 
 var main = {
     count: 1,
-    colors: ['red', 'blue', 'purple', 'green', 'yellow', 'brown', 'black', 'pink', 'orange', 'aqua', 'grey', 'teal', 'olive', 'fuchsia'],
-    getHeight: function(block, top){        //получаем здесь высоту клика и блок с которым работаем. Вычисляем высоту начального и нового блоков.
-        if(main.colors.length != 0) {
-            var heightOfNewBlock = $('body').height();
-            $(block).height( (top - $(block).offset().top) / ($('body').height()/100) + '%');
-            for(var i = 0; i < $('div').length; i++){
-                heightOfNewBlock -= $('div').eq(i).height();
-            }
-            main.createBlock($(block), heightOfNewBlock);
-        }else{
-            alert('Цветов больше нет!');
+    getHeight: function(block, y, x){        //получаем здесь стартовый блок с координатами клика. Вычисляем границы и размеры блоков, изменяем.
+        var top = $(block).offset().top,
+            bottom = top + $(block).height(),
+            left = $(block).offset().left,
+            right = left + $(block).width(),
+            pageHeight = $('body').height(),
+            pageWidth = $('body').width(),
+            fromTop = y - top,
+            fromLeft = x - left,
+            fromRight = right - x,
+            fromBottom = bottom - y,
+            heightOfNewBlock,
+            widthOfNewBlock;
+
+        if(fromTop <= fromLeft && fromTop <= fromRight && fromTop <= fromBottom){
+            heightOfNewBlock = $(block).height() - fromBottom;
+            widthOfNewBlock = $(block).width();
+            $(block).height(Math.round(($(block).height()-heightOfNewBlock) / (pageHeight/100)) + '%');
+            main.createBlock($(block), heightOfNewBlock, widthOfNewBlock, 'before');
         }
+        else if(fromBottom < fromLeft && fromBottom < fromRight && fromBottom < fromTop){
+            heightOfNewBlock = $(block).height() - fromTop;
+            widthOfNewBlock = $(block).width();
+            $(block).height(Math.round(fromTop / (pageHeight/100)) + '%');
+            main.createBlock($(block), heightOfNewBlock, widthOfNewBlock, 'after');
+        }
+        else if(fromRight < fromLeft && fromRight < fromBottom && fromRight < fromTop){
+            widthOfNewBlock = Math.round($(block).width() - fromLeft);
+            heightOfNewBlock = $(block).height();
+            $(block).width(Math.round(fromLeft / (pageWidth/100)) + '%');
+            main.createBlock($(block), heightOfNewBlock, widthOfNewBlock, 'after');
+        }else{
+            widthOfNewBlock = Math.round($(block).width() - fromRight);
+            heightOfNewBlock = $(block).height();
+            $(block).width(Math.round(($(block).width()-widthOfNewBlock) / (pageWidth/100)) + '%');
+            main.createBlock($(block), heightOfNewBlock, widthOfNewBlock, 'before');
+        }
+
     },
-    createBlock: function(block, height){       //Создаем новый блок
+    createBlock: function(block, height, width, position){       //Создаем новый блок
         var color = main.getColor(),
-            currentHeight = height / ($('body').height()/100);
+            currentHeight = height / ($('body').height()/100),
+            currentWidth = width /($('body').width()/100) ;
         ++main.count;
-        $('<div>', {
-            'data-index': main.count,
-            css: {
-                width: '100%',
-                height: currentHeight + '%',
-                backgroundColor: color
-            },
-            on: {
-                click: function(e){
-                    var top = e.pageY;
-                    main.getHeight($(this), top);
+        switch (position) {
+            case 'after': $('<div>', {
+                    'data-index': main.count,
+                    css: {
+                        width: Math.round(currentWidth) + '%',
+                        height: Math.round(currentHeight) + '%',
+                        backgroundColor: color
+                    },
+                    on: {
+                        click: function (e) {
+                            var y = e.pageY,
+                                x = e.pageX;
+                            main.getHeight($(this), y, x);
+                        }
+                    }
+                }).insertAfter(block);
+            break;
+            case 'before': $('<div>', {
+                'data-index': main.count,
+                css: {
+                    width: Math.round(currentWidth) + '%',
+                    height: Math.round(currentHeight) + '%',
+                    backgroundColor: color
+                },
+                on: {
+                    click: function (e) {
+                        var y = e.pageY,
+                            x = e.pageX;
+                        main.getHeight($(this), y, x);
+                    }
                 }
-            }
-        }).insertAfter(block);
+            }).insertBefore(block);
+                break;
+        }
 
         main.sendInfo(main.count, color, height);
     },
     getColor: function(){       //Получаем рандомный цвет
-        //TODO можно дописать рандомизатор на RGB
-        var colorCount = Math.floor(Math.random() * main.colors.length),
-            color = main.colors[colorCount];
-        main.colors.splice(colorCount, 1);
+        var color = '#' + Math.round((Math.random() * (999999 - 100000) + 100000));
         return color;
     },
     sendInfo: function(index, color, height){ //Отправка данных на сервер
-        var formData = {
-            'index':index,
-            'color':color,
-            'width':$('body').width(),
-            'height':height
-        };
+       formData.all.push({
+           'index':index,
+           'color':color,
+           'width':$('body').width(),
+           'height':height
+       });
         $.ajax({
             url:'dataparser.php',
             type:'POST',
@@ -67,4 +112,8 @@ var main = {
             }
         });
     }
+};
+
+var formData = {
+    all: []
 };
